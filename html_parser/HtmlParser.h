04b3ccee501a54e33793defb5a8bb6e5b486ffab
Kam Bielawski <kamtb28@gmail.com>
2021-01-02 14:08:23 -0600

    exclamation tags (comments & doctype)

--- html_parser/HtmlParser.h
+++ html_parser/HtmlParser.h
@@ -5,13 +5,13 @@
 #include <stdio.h>
 
 struct TokenAttribute {
-  std::string attr;
-  std::string value;
+    std::string attr;
+    std::string value;
 };
 
 struct HtmlToken {
-  std::string token_type; // type of token
-  std::string token_content; // inside token (i.e. 'hello' in "<p>hello</p>")
+    std::string token_type; // type of token
+    std::string token_content; // inside token (i.e. 'hello' in "<p>hello</p>")
   TokenAttribute token_attributes[10];
 };
 
@@ -24,31 +24,35 @@ struct TokenStackNode {
 #define HTML_PARSER_H
 
 class HtmlParser {
-private:
-char *html_string;
-int string_pos;
-int string_length;
-int stack_size;
-char curChar;
-TokenStackNode *token_stack; 
+  private:
+    char *html_string;
+    int string_pos;
+    int string_length;
+    int stack_size;
+    char curChar;
+    TokenStackNode *token_stack; 
+    std::string doctype;
 
-public:
-HtmlParser();
-~HtmlParser();
-void parseHtmlString (char *html_string);
-void parseHtmlFromFile (const char *file_name);
+  public:
+    HtmlParser();
+    ~HtmlParser();
+    void parseHtmlString (char *html_string);
+    void parseHtmlFromFile (const char *file_name);
 
-protected:
-int getStrLen(char *string) const;
-void getNextChar ();
-void getToken ();
-void skipWhitespace();
-char peekNext() const;
-std::string readUntil(char until);
+  protected:
+    int getStrLen(char *string) const;
+    void getNextChar ();
+    void getToken ();
+    void skipWhitespace();
+    char peekNext() const;
+    std::string readUntil(char until);
+    void ignoreComment ();
+    bool checkValidDoctype () const;
+    void parseDoctype();
 
-// token stack management
-void tokenStackPush(struct HtmlToken token);
-void tokenStackPop();
+    // token stack management
+    void tokenStackPush(struct HtmlToken token);
+    void tokenStackPop();
 
 };
 
